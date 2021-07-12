5f7061630ae2843e04c48eeb190ebeb4c7172048
Julia Naomi <jnboeira@outlook.com>
2021-07-11 22:05:45 -0300

    draw tank_basetexture

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,11 +1,18 @@
-use macroquad::{prelude::*};
+use macroquad::prelude::*;
 
 #[macroquad::main("Tank Battle Ground")]
 async fn main() {
+    let tank_base: Texture2D = load_texture("../assets/TankBase.png").await.unwrap();
+    
     loop {
         clear_background(BEIGE);
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_texture(
+            tank_base,
+            screen_width() / 2. - tank_base.width() / 2.,
+            screen_height() / 2. - tank_base.height() / 2.,
+            WHITE,
+        );
 
         next_frame().await
     }
