915116d5f5a2b2c0bdab08d5ebbe8ad3403e0864
Julia Naomi <jnboeira@outlook.com>
2021-07-14 12:45:27 -0300

    add tank base image

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,15 +1,48 @@
+use std::path;
+
 use ggez;
 use ggez::conf::WindowMode;
 use ggez::conf::WindowSetup;
 use ggez::event;
 use ggez::graphics;
+use ggez::graphics::screen_coordinates;
+use ggez::graphics::Color;
+use ggez::nalgebra as na;
+use ggez::Context;
 
-struct MainState {}
+pub struct Tank {
+    position: na::Point2<f32>,
+    texture: graphics::Image,
+}
+
+impl event::EventHandler for Tank {
+    fn update(&mut self, _ctx: &mut ggez::Context) -> ggez::GameResult {
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
+        graphics::draw(
+            ctx,
+            &self.texture,
+            (self.position, 0.0, Color::from_rgb(255, 255, 255)),
+        )?;
+        Ok(())
+    }
+}
+
+struct MainState {
+    tank: Tank,
+}
 
 impl MainState {
-    fn new() -> MainState {
-        let s = MainState {};
-        s
+    fn new(ctx: &mut Context) -> ggez::GameResult<MainState> {
+        let sc = screen_coordinates(ctx);
+        let tank = Tank {
+            position: na::Point2::from([sc.w / 2., sc.h / 2.]),
+            texture: graphics::Image::new(ctx, "/TankBase.png")?,
+        };
+        let s = MainState { tank };
+        Ok(s)
     }
 }
 
@@ -20,14 +53,16 @@ impl event::EventHandler for MainState {
 
     fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
         graphics::clear(ctx, [0.83, 0.69, 0.51, 1.0].into());
-
+        self.tank.draw(ctx)?;
         graphics::present(ctx)?;
         Ok(())
     }
 }
 
 pub fn main() -> ggez::GameResult {
+    let resources_dir = path::PathBuf::from("../resources");
     let cb = ggez::ContextBuilder::new("tank_battle", "naomijub")
+        .add_resource_path(resources_dir)
         .window_setup(WindowSetup {
             title: "Tank Battle Ground".to_owned(),
             samples: ggez::conf::NumSamples::Zero,
@@ -49,6 +84,6 @@ pub fn main() -> ggez::GameResult {
         });
 
     let (ctx, event_loop) = &mut cb.build()?;
-    let mut state = MainState::new();
+    let mut state = MainState::new(ctx)?;
     event::run(ctx, event_loop, &mut state)
 }
